16cda94e492797f715092d95a945eaae9307ea82
Tim Fox <tim@elephantscale.com>
2021-01-29 15:46:35 -0500

    Added the map example

--- learnjava/src/main/java/com/example/learnjava/LearnjavaApplication.java
+++ learnjava/src/main/java/com/example/learnjava/LearnjavaApplication.java
@@ -1,10 +1,19 @@
 package com.example.learnjava;
 
 
+import java.util.HashMap;
+import java.util.ArrayList;
+
 public class LearnjavaApplication {
 
 	public static void main(String[] args) {
 
+		System.out.println("Hello World!");
+
+
+		// Map Example
+		MapExample example = new MapExample();
+		example.ShowMap();
 	}
 
 }
---
+++ learnjava/src/main/java/com/example/learnjava/MapExample.java
@@ -0,0 +1,23 @@
+package com.example.learnjava;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MapExample {
+    public void ShowMap() {
+
+        ArrayList<HashMap<String, String>> students = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> tim = new HashMap<String, String>();
+
+
+        tim.put("name", "Tim");
+
+        tim.put("address", "123 Main Street");
+
+        students.add(tim);
+
+
+        System.out.println(tim.get("name"));
+    }
+
+}
