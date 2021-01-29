9cafe11a34268c0fb2adc2ec416e4cf95f953b00
Tim Fox <tim@elephantscale.com>
2021-01-29 14:40:49 -0500

    Added calculator.java with example

--- calculator/src/calculator/calculator.java
+++ calculator/src/calculator/calculator.java
@@ -1,5 +1,29 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class calculator {
+	
+	public static void main(String[] args) {
+	
+		
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+
+		
+        Scanner in = new Scanner(System.in); 
+        
+        String s = "Welcome to Calculator!";
+        System.out.println(s);
+        while ((s = in.nextLine()) != "") {
+            System.out.println(s);
+            String[] components = s.split(" ");
+            
+
+        }
+        
+	}
+	
 
 }
