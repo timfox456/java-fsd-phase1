99ff356ee2374ee5ff9a6b9278e1b6926a36d0ed
Tim Fox <tim@elephantscale.com>
2021-02-01 16:26:38 -0500

    Added stub for emailvalidate

---
+++ emailvalidate/src/main/java/com/example/emailvalidate/EmailvalidateApplication.java
@@ -0,0 +1,17 @@
+package com.example.emailvalidate;
+
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class EmailvalidateApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+
+
+	}
+
+}
