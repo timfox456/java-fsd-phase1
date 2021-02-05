a46ba230eb592deab86d9de741d2637630d39ba2
Tim Fox <tim@elephantscale.com>
2021-02-05 17:17:45 -0500

    Added array

--- learnjava/src/main/java/com/example/learnjava/ArrayExample.java
+++ learnjava/src/main/java/com/example/learnjava/ArrayExample.java
@@ -1,5 +1,9 @@
 package com.example.learnjava;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ArrayExample {
 
     private int checkerboard[][] = new int[8][8];
@@ -55,6 +59,30 @@ public class ArrayExample {
         setupGame();
         printBoard();
     }
+
+    public void RotateArray(int[] nums, int k)
+    {
+        if(k > nums.length)
+            k = k % nums.length;
+        int[] result = new int[nums.length];
+        for(int i=0; i < k; i++){
+            result[i] = nums[nums.length-k+i];
+        }
+        int j=0;
+        for(int i=k; i<nums.length; i++){
+            result[i] = nums[j];
+            j++;
+        }
+        System.arraycopy( result, 0, nums, 0, nums.length );
+
+    }
+
+    public int KthSmallestNumber(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        return nums[k];
+    }
+
 
 
 
--- learnjava/src/main/java/com/example/learnjava/LearnjavaApplication.java
+++ learnjava/src/main/java/com/example/learnjava/LearnjavaApplication.java
@@ -174,5 +174,19 @@ public class LearnjavaApplication {
 		VehicleExample vehicleExample = new VehicleExample();
 		vehicleExample.GetVehicles();
 
+
+		System.out.println("-----------------");
+		System.out.println("Array Example (Part 2):\n");
+
+		int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+		arrayExample.RotateArray(arr, 5);
+
+		for(int i=0;i<arr.length;i++){
+			System.out.print(arr[i]+" ");
+		}
+
+		System.out.println(arrayExample.KthSmallestNumber(arr, 3));
+
+
 	}
 }
